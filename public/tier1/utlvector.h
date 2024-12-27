9a3f5b3939692eb8a80d98122b7296743f72fff8
A1m` <33463136+A1mDev@users.noreply.github.com>
2024-12-28 02:55:09 +0700

    [L4D2] Add class 'CUtlMemoryConservative' (#287)
    * Add debug functions for keyvalues
    
    * Add CUtlMemoryConservative

--- public/tier1/utlvector.h
+++ public/tier1/utlvector.h
@@ -226,6 +226,22 @@ public:
 
 
 //-----------------------------------------------------------------------------
+// The CUtlVectorConservative class:
+// A array class with a conservative allocation scheme
+//-----------------------------------------------------------------------------
+template< class T >
+class CUtlVectorConservative : public CUtlVector< T, CUtlMemoryConservative<T> >
+{
+	typedef CUtlVector< T, CUtlMemoryConservative<T> > BaseClass;
+public:
+
+	// constructor, destructor
+	explicit CUtlVectorConservative( int growSize = 0, int initSize = 0 ) : BaseClass( growSize, initSize ) {}
+	explicit CUtlVectorConservative( T* pMemory, int numElements ) : BaseClass( pMemory, numElements ) {}
+};
+
+
+//-----------------------------------------------------------------------------
 // The CCopyableUtlVector class:
 // A array class that allows copy construction (so you can nest a CUtlVector inside of another one of our containers)
 //  WARNING - this class lets you copy construct which can be an expensive operation if you don't carefully control when it happens
